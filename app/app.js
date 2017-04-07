17626a53fe8c4ab1578b0238377463f04959f015
Jacek Ławniczak <jacekelgda@gmail.com>
2017-04-07 17:36:30 +0200

    Conversation after hearing phrase --wip #2

--- app/app.js
+++ app/app.js
@@ -1,24 +1,8 @@
 import bodyParser from 'body-parser'
-import express from 'express'
 import * as botHandler from './handler/bot'
 
-const app = express()
-
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-
 botHandler.listener.hears(['vacation', 'holiday', 'ooo', 'time off'], ['ambient'], (bot, message) => {
-  console.log(`User: ${message.user} want to take a vacation`)
   botHandler.startVacationRequestConversation(message.user)
-  // start conversation with user
-  //    ask if he wants to file in vacation request
-  //    yes > what is the start date, then what is the end date
-  //    then ask if ok and change status
 })
 
 var port = process.env.PORT || 3000
-app.listen(port)
-
-app.get('/', async function (req, res) {
-  res.send('Hello')
-})
--- app/handler/bot.js
+++ app/handler/bot.js
@@ -15,13 +15,57 @@ const bot = listener.spawn({
 
 const startVacationRequestConversation = (user) => {
   bot.startPrivateConversation({user: user}, (err, convo) => {
-    if (err) {
-        console.log('error', err)
-    } else {
-      convo.ask('Are you planning vacation?', (message, convo) => {
-        convo.next()
-      })
-    }
+    convo.addQuestion('Noice! And when are you back at X-Team?',[
+      {
+        default: true,
+        callback: function(response,convo) {
+          console.log('Vacation ends: ', response.text)
+          convo.next();
+        }
+      }
+    ],{},'end_vacation');
+
+    convo.addQuestion('Great! What time shall your vacation start ?',[
+      {
+        default: true,
+        callback: function(response,convo) {
+          console.log('Vacation starts: ', response.text)
+          convo.gotoThread('end_vacation');
+        }
+      }
+    ],{},'yes_thread');
+
+    convo.addMessage({
+        text: 'Right ... Sorry! Must have missheard something. Pleas don\'t tell my boss...',
+    },'no_thread');
+
+    convo.addMessage({
+        text: 'Sorry I did not understand.',
+        action: 'default',
+    },'bad_response');
+
+    convo.ask('I can\'t help to notice that you have mentioned something about vacation ... Is that correct?', [
+        {
+            pattern: bot.utterances.yes,
+            callback: (response, convo) => {
+                convo.gotoThread('yes_thread');
+            },
+        },
+        {
+            pattern: bot.utterances.no,
+            callback: (response, convo) => {
+                convo.gotoThread('no_thread');
+            },
+        },
+        {
+            default: true,
+            callback: (response, convo) => {
+                convo.gotoThread('bad_response');
+            },
+        }
+    ]);
+
+    convo.activate();
   })
 }
 
