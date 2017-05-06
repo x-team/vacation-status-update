808a62d96abda3c91e49ffc144b8395982650a69
Jacek Lawniczak <jacekelgda@gmail.com>
2017-05-06 10:47:52 +0200

    Wakup controller for heroku free dyno (#52)

--- controllers/index.js
+++ controllers/index.js
@@ -2,11 +2,13 @@ import express from 'express'
 import slashCommandController from './slashCommandController'
 import authController from './authController'
 import interactiveMessagesController from './interactiveMessagesController'
+import wakeController from './wakeController'
 
 const router = new express.Router()
 
 router.use(slashCommandController)
 router.use(authController)
 router.use(interactiveMessagesController)
+router.use(wakeController)
 
 export default router
