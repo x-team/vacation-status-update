dae1bc15ba74b24e794e135af932429d6ac4a357
Jacek Ławniczak <jacekelgda@gmail.com>
2017-05-06 10:44:00 +0200

    Wakup controller for heroku free dyno

---
+++ controllers/wakeController.js
@@ -0,0 +1,9 @@
+import express from 'express'
+
+const router = new express.Router()
+
+router.get('/wakeup', async function (req, res) {
+  res.send('I wasn\'t sleeping ...')
+})
+
+export default router
