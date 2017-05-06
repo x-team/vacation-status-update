a93cef6bb45c93d80a808cfdd8484d2b39c14688
Jacek Ławniczak <jacekelgda@gmail.com>
2017-05-06 10:56:18 +0200

    Allow post on wake controller

--- controllers/wakeController.js
+++ controllers/wakeController.js
@@ -6,4 +6,8 @@ router.get('/wakeup', async function (req, res) {
   res.send('I wasn\'t sleeping ...')
 })
 
+router.post('/wakeup', async function (req, res) {
+  res.send('I wasn\'t sleeping ...')
+})
+
 export default router
