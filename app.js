46514f2cfb5c46ee1444c144da5949ac1b820687
Jacek Ławniczak <jacekelgda@gmail.com>
2017-06-29 17:14:50 +0200

    expanding bots dictionary --closes #85

--- app.js
+++ app.js
@@ -20,14 +20,25 @@ setupTeams()
 
 cronUtil.startVacationStartCheckJob()
 cronUtil.startVacationEndCheckJob()
-// cronUtil.bumpDndStatusForUsersOnVacation()
-
 async function setupTeams() {
   await storeHandler.init()
   await storeHandler.setupDevTeam()
   const tokens = await storeHandler.getAllTokens()
   botHandler.resumeAllConnections(tokens)
-  botHandler.listener.hears(['vacation', 'holiday', 'ooo', 'time off'], ['ambient'], (bot, message) => {
+  const phrases = [
+    'vacation',
+    'holiday',
+    'ooo',
+    'time off',
+    'out of office',
+    /i(am|’m|'m') (off|out) (next|from) (\w+-\w+|\w+)(\sto\s\w+)?/gim,
+    /(i|we)(\swill|'ll|’ll) be (out|off) (most of the|next) (day|week|month|year|time|couple of days)/gim,
+    /(i|we)(\swill|'ll|’ll) be (out|off) (the day after\s)?tomorrow/gim,
+    /(i|we)(\swill|'ll|’ll) be (out|off) in (\w+|\d+) days/gim,
+    /(i|we)(\swill|'ll|’ll) be in \w+ (the|from|in|next) \w+|(\d+\w+)/gim,
+    /(i|we)(\swill|'ll|’ll|\sneed to) take (some|a) time off/gim,
+  ]
+  botHandler.listener.hears(phrases, ['ambient'], (bot, message) => {
     botHandler.startVacationRequestConversation(bot, message.user)
     botHandler.markMessageWithEmoji(bot, message)
   })
