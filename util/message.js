5466d405423c2b216c0ad05d821085a6cfa86405
Serhii Baraniuk <kenitive@gmail.com>
2017-10-11 13:08:31 +0300

    add tests for message utils (#105)

--- util/message.js
+++ util/message.js
@@ -1,5 +1,5 @@
 export const extractMentionedUsers = (message) => {
-    const matches = message.text.match(/<@\w*>/gi)
+    const matches = message.text.match(/<@\w*>/gi) || []
     const users = matches.map((text) => {
         return text.replace(/^<@/i, '').replace(/>$/i, '')
     })
